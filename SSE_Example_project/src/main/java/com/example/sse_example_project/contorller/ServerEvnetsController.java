78c803cb3786ea8bf21af46d96a0973ad1ea1975
UMK <haga5032@naver.com>
2023-05-14 17:36:16 +0900

    예제 만들기 완성

--- SSE_Example_project/src/main/java/com/example/sse_example_project/contorller/ServerEvnetsController.java
+++ SSE_Example_project/src/main/java/com/example/sse_example_project/contorller/ServerEvnetsController.java
@@ -1,34 +1,46 @@
 package com.example.sse_example_project.contorller;
 
 
-import com.example.sse_example_project.dto.ServerSentEvent;
-import org.springframework.http.MediaType;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.stream.Stream;
+import java.io.Writer;
 
 @RestController
 @RequestMapping("/events")
 public class ServerEvnetsController {
 
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<ServerSentEvent<String>> getEevnts() throws IOException {
+//    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+//    public Flux<ServerSentEvent<String>> getEevnts() throws IOException {
+//
+//        Stream<String> lines = Files.lines(Path.of("C:/SSE_Example/SSE_Example_project/build.gradle"));
+//
+//        Flux.fromStream(lines)
+//                .filter(line -> !line.isBlank())
+//                .map(line -> {
+//                    ServerSentEvent.builder()
+//                            .id(String.valueOf(counter.getAndIncrement()))
+//                            .data()
+//                })
+//        return null;
+//    }
 
-        Stream<String> lines = Files.lines(Path.of("C:/SSE_Example/SSE_Example_project/build.gradle"));
+    @GetMapping("/go")
+    public void sse(final HttpServletResponse response) throws IOException, InterruptedException {
+        response.setContentType("text/event-stream");
+        response.setCharacterEncoding("UTF-8");
 
-        Flux.fromStream(lines)
-                .filter(line -> !line.isBlank())
-                .map(line -> {
-                    ServerSentEvent.builder()
-                            .id(String.valueOf(counter.getAndIncrement()))
-                            .data()
-                })
-        return null;
+        Writer writer = response.getWriter();
+
+        for (int i = 0; i < 5; i++) {
+            writer.write("data: " + System.currentTimeMillis() + "\n\n");
+            writer.flush(); // 꼭 flush 해주어야 한다.
+            Thread.sleep(1000);
+        }
+
+        writer.close();
     }
 }
